e77c7a9e3ba885a6b23be8a3a04a665087bb5112
KatoMono Enkeli <k4t0mono@gmail.com>
2018-02-14 18:29:22 -0200

    Lession 071: Validators, types, defaults

--- server/server.js
+++ server/server.js
@@ -8,31 +8,51 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
 	text: {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
 	},
 
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false
 	},
 
 	completedAt: {
-		type: Number
+		type: Date,
+		default: Date.now()
 	}
 });
 
-var newTodo = new Todo({ text: 'Config Void' });
-
-var newTodo2 = new Todo({ 
-	text: 'Config Arch',
-	completed: true,
-	completedAt: Date.now()
+var User = mongoose.model('User', {
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	}
 });
 
-// newTodo.save().then((doc) => {
-newTodo2.save().then((doc) => {
-	console.log('Saved todo', doc);
+var newUser = new User({ email: 'k4t0mono@terminus.io' });
+newUser.save().then((doc) => {
+	console.log('Saved user', JSON.stringify(doc, undefined, 2));
 
 }, (err) => {
-	console.log('Unable to save todo', err);
-});
+	console.log('Unable to save user', err);
+})
+
+//var newTodo = new Todo({ text: 'Config Void' });
+
+//var newTodo2 = new Todo({ 
+	//text: 'Config Arch2',
+//});
+
+//newTodo.save().then((doc) => {
+//newTodo2.save().then((doc) => {
+	//console.log('Saved todo', JSON.stringify(doc, undefined, 2));
+
+//}, (err) => {
+	//console.log('Unable to save todo', err);
+//});
 
